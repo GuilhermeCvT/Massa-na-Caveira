f956cbba3b7574e2738d24c294d4e6708b295373
Guilherme Silva <guilhermecvt@hotmail.com>
2022-02-26 11:48:35 -0300

    Versao 0.1.4

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,16 +1,50 @@
 class Admins::UsersController < AdminsController
+  before_action :verify_password, only: [:update]
+  before_action :set_user, only: [:edit, :update]
+  before_action :create, only: [:params_user]
+
   def index
-    @users = User.all
+    @users = User.all.order(:id)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params_user)
+
+    if @user.save
+      redirect_to admins_users_path, notice: "Usuário Cadastrado com sucesso!"
+    else 
+      render :new # renderiza a página de edit novamente 
+    end
   end
 
   def edit
-    @user = User.find(params[:id])
-    @entrances = Entrance.all
-    @companies = Company.all
+
   end
 
   def update
+    if @user.update(params_user)
+      redirect_to admins_users_path, notice: "Usuário atualizado com sucesso!"
+    else 
+      render :edit # renderiza a página de edit novamente 
+    end
+  end
+
+  private
+  def params_user
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :company_id, :entrance_id)
+  end
+
+  def set_user
     @user = User.find(params[:id])
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :company, :entrance)
+  end
+
+  def verify_password
+    if params[:user][:password].blank? && params[:password_confirmation].blank?
+      params[:user].extract!(:password, :password_confirmation)
+    end
   end
 end
